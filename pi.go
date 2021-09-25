d4187c4b2e1e5f6e20255699a49bf691c61f44c6
aravindas4 <ash.g.education@gmail.com>
2021-09-25 19:20:03 +0530

    constants have implicit type conversion: int, rune, float, complex

--- pi.go
+++ pi.go
@@ -4,5 +4,16 @@ import "fmt"
 
 func main() {
 	var π = 22 / 7.0
+	// This do not compile
+	// var a, b = 22, 7.0
+	// pi := a / b
 	fmt.Println(π)
+	const a, b = 22, 7.0
+	pi := a / b
+	fmt.Println(pi)
+	const myUint8 = 1000000000000000000000000000000000000000000000
+	// Invalid
+	// const myUint int64 = 1000000000000000000000000000000000000000000000
+	var answer = 3 * 0.333
+	_ = answer
 }
